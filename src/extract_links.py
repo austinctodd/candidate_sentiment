bac771a5ff2c225a6ab224c4d70e891a4c1d24d9
austin.todd <austin.todd@tu-berlin.de>
2016-07-28 17:52:48 +0200

    Added speech text extraction

--- src/extract_links.py
+++ src/extract_links.py
@@ -13,9 +13,34 @@ html_page=urllib2.urlopen('http://www.presidency.ucsb.edu/nomination.php')
 soup=BeautifulSoup(html_page)
 
 # Loop through all the lines that link to speech files - extract speech web address and speaker
-speaker=[]
-pagelink=[]
+speakers=[]
+speeches=[]
 for link in soup.findAll('a',attrs={'href': re.compile("^http://www.presidency.ucsb.edu/ws/index")}):
-	speaker.append(re.sub(r'\W+',' ',link.string))
-	pagelink.append(link.get('href'))
+	speakers.append(re.sub(r'\W+',' ',link.string))
+
+    # Open new Beautifulsoup object with contained link
+    soup2=BeautifulSoup(urllib2.urlopen(link.get('href')))
+    
+    # Initialize string that will contain transcript
+    speech=''
+
+    # Extract transcript - first find all <p> references
+    for s in soup2.findAll('p'):
+        
+        # Not all <p> instances created equal
+        # May miss some content if the paragraph has a formatting object
+        #     e.g. <i>The President.</i>
+        #          <i>Audience Members</i>
+        #          <i>Laughter</i>
+        #          <i>applause</i>
+        #          <i>laughter and applause</i>
+        if s.i:
+            if any(b in s.i.text.lower() for b in ['laughter','president','applause']):
+                s.i.decompose()
+                speech=speech+s.text+' '
+        else:
+            speech=speech+s.text+' '
+
+    speeches.append(speech)
+
 
